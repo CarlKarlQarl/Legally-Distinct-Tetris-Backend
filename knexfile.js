0e782cde41c440303c57c599c8cd49b01c95bc13
CarlKarlQarl <karlmatthes@gmail.com>
2020-03-16 14:48:08 -0600

    Correcting knexfile to include production

--- knexfile.js
+++ knexfile.js
@@ -5,5 +5,13 @@ module.exports = {
   development: {
     client: 'pg',
     connection: "postgres:///tetris_backend_db1"
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + "/migrations"
+    }
   }
 };
